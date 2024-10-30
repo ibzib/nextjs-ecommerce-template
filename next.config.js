a681d9ad52deb798137b4112601b86232928d624
Kyle Weaver <kylecweaver@gmail.com>
2024-10-29 23:51:43 -0700

    Update next config
    - no image loader
    - no strict mode 🤠

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  reactStrictMode: true,
   images: {
-    loader: "akamai",
-    domains: [
-      "picsum.photos",
-      "loremflickr.com",
-      "placeimg.com",
-      "source.unsplash.com",
-    ],
   },
 };
